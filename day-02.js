e40624f16a411873a4ce6b0c1d2051648c5b27bd
Faran <faranpannu02@gmail.com>
2026-03-14 07:02:52 +0500

    js precctoce code

---
+++ day-02.js
@@ -0,0 +1,36 @@
+console.log("Hello Javascript");
+const name="Faran";
+let age=19;
+let city="Faisalabad";
+console.log(name+" "+age+" "+city);
+
+let a=10;
+let b=30;
+let sum=a+b;
+console.log(sum);
+let num=20;
+if(num%2==0){
+    console.log("Number is even");
+}else{
+    console.log("number is odd");
+}
+
+let marks=49;
+if(marks>=50){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
+function greet(){
+    alert("Welcome to Javascript");
+}
+function sum( a,b){
+ 
+}
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
