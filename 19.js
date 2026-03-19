2608a3fc984a10823a16abf95b1c4f6200ca31f2
Faran <faranpannu02@gmail.com>
2026-03-19 11:38:03 +0500

    js prectice code

---
+++ 19.js
@@ -0,0 +1,33 @@
+// variables are containers for storing data
+//variable can be declared in four ways
+//New Methode
+//1- let    2- const
+//older Method
+//1- var    2-automaticaly
+
+// examples of new Mathode for creating variables
+let a=10;
+let b=5;
+let c=a+b;
+console.log(c);
+
+const x=2;
+const y=5;
+const z=x+y;
+console.log(z);
+
+
+//name of variables are called identifiers
+//it must be descriptive
+//names are case-sensitive
+// reserved words are not used as name
+// numbers are not allowed to be the first letter of the name
+//(_) javascripts treat like a letter
+
+let _name="Faran";
+//semicoln at the end is optional
+
+//($) javascript also deals a dollar sign to like a letter
+let $name="Faran Zafar";
+let $$$=2;
+let $mony=200;
