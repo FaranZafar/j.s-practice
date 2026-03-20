17e7cb6a05178056a58aaf50835ddabda4378a5b
Faran <faranpannu02@gmail.com>
2026-03-20 16:30:00 +0500

    js prectice code

--- 19.js
+++ 19.js
@@ -31,3 +31,56 @@ let _name="Faran";
 let $name="Faran Zafar";
 let $$$=2;
 let $mony=200;
+// typeof keyword is used to find the type of keyword
+console.log(typeof _name);
+
+// operators in js
+// =,+,-,*,/,<,>,<=,>=,==
+//assignment operator
+//=,+=,-=,*=,/=,%=,**=
+let assignmentop=3
+// arithmatic operators
+//+,-,*,/,**,%,++,--
+//addition operator
+let num1=12;
+let num2=12;
+let addop=num1+num2; //answer is 24
+// multiplication operator is used to multiply numbers
+let multiplicationop=num1*num2;  // answer is 144
+//devision operator
+let devisionop=num1*num2;  // answer is 1
+//subtraction operator
+let subop=num1*num2;  // answer is 0
+//subtraction operator
+let exponop=num1**num2;  // answer is 12**12
+//mudulus operator
+let muduloop=num1%num2;  // answer is reminder
+
+//++ increment operator
+//-- decrement operator
+
+//comparison operator
+//1 ==   equal value
+ a==12
+//2 === equal value and equal data type
+a===12
+//3 !=  not equal
+a!=12
+//4 !== not equal value and not equal to data type
+a!==12
+//5 >  greater than
+a>12
+//6 < less than
+a<12
+//7 >= greater than are equal
+a>=12
+//8 <= less than or  equal to
+a<=12
+
+//logical operators
+//1 &&  and operator
+a<=12 && a>=6
+//2 ||  or operator
+a<=12 || a>=6
+//3 !   not operator
+a<=12 != a>=6
---
+++ loops.js
@@ -0,0 +1,41 @@
+for(let i=0;i<12;i++){
+    console.log("Eid Mubarak");
+}
+for(let i=0;i<10;i++){
+   console.log("number is: "+ i);
+}
+//even numbers 
+for(let i=0;i<=50;i++){
+    if(i%2==0){
+        console.log(i);
+    }else{
+        console.log("odd number");
+    }
+}
+//reveerse counting
+for(let i=20;i>=0;i--){
+    console.log(i);
+}
+//sum of numbers 
+let sum=0;
+for(let i=0;i<=10;i++){
+    sum+=i;
+
+}
+console.log("sum of first 10 numbers :"+sum);
+//multiplication table
+
+for(let i=1;i<=10;i++){
+    console.log("5 x "+i+" : "+5*i);
+}
+//factorial  of numbers
+let fact=1;
+for(let i=5 ;i>=1;i--){
+    fact*=i;
+}
+console.log("factorial of 5 :"+fact);
+
+// let num=12345;
+// let count=num.toString(num).length
+
+//prime number check
