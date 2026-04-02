fa0b2429a2f9531b9dd210d2f0de549dfa1cbbb7
Faran <faranpannu02@gmail.com>
2026-04-03 04:22:41 +0500

    js prectice code

---
+++ while-loop.js
@@ -0,0 +1,38 @@
+// js have two types of while loop
+//1 -> while loop
+// through a block of code as long as
+// a specified condition is ture
+// while(condition){
+//     //code block
+// }
+ i=0;
+// while (i < 10) {
+//   console.log( "The number is " + i);
+//   i++;
+// }
+// while(i<=10){
+//     console.log("number :"+i);
+//     i++;
+// }
+//even number
+// while(i<=20){
+//  if(i%2==0){
+//     console.log(i);
+//  }
+//  i++;
+// }
+//print odd numbers
+
+while(i<=15){
+    if(i%2!=0){
+        console.log(i);
+    }
+    i++;
+}
+//2 -> do while loop
+// it runs one time before checking the condition
+// do{
+//     console.log("number is :"+i);
+//     i++;
+// }while(i>10);
+
